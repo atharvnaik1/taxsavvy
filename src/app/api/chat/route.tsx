8bb6060edb8a74b28b6df58c6197402ab0959fb0
atharvnaik1 <atharv1599@gmail.com>
2024-04-13 13:01:52 +0530

    api add

--- src/app/api/chat/route.tsx
+++ src/app/api/chat/route.tsx
@@ -2,9 +2,9 @@ import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 
 const openai = new OpenAI({
-  apiKey: process.env.OPEN_API_KEY || '',
+  apiKey: process.env.OPENAI_API_KEY || '',
 });
-
+// OPENAI_API_KEY
 export const runtime = 'edge';
 
 export async function POST(
